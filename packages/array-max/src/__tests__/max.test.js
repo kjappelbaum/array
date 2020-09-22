36e646dfeed1472b360e84334f2ff9806d52c399
Kevin <kevin.jablonka@epfl.ch>
2020-09-22 23:16:23 +0200

    chore: Incorporated feedback
    - int -> Number 
    - from -> fromIndex, to -> toIndex
    - check if int

--- packages/array-max/src/__tests__/max.test.js
+++ packages/array-max/src/__tests__/max.test.js
@@ -12,10 +12,35 @@ describe('array-max', () => {
     expect(max([1, 2])).toBe(2);
     expect(max([1, 2, 1])).toBe(2);
     expect(max([3, 2, 1])).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 0, toIndex: 2 })).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 0, toIndex: 3 })).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 1, toIndex: 3 })).toBe(2);
+    expect(max([3, 2, 1], { fromIndex: 0, toIndex: 2 })).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 2, toIndex: 3 })).toBe(1);
     expect(max(typedArray)).toBe(3);
+    expect(max(typedArray, { fromIndex: 0, toIndex: 2 })).toBe(2);
+    expect(max(typedArray, { fromIndex: 0, toIndex: 3 })).toBe(3);
   });
   it('should throw on invalid value', () => {
     expect(() => max()).toThrow(/input must be an array/);
     expect(() => max([])).toThrow(/input must not be empty/);
+    expect(() => max([1, 2, 3], { fromIndex: -1, toIndex: 2 })).toThrow(
+      /start index must be a integer greater than 0 and smaller equal than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 4, toIndex: 2 })).toThrow(
+      /start index must be a integer greater than 0 and smaller equal than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 1, toIndex: 0 })).toThrow(
+      /to index must be a integer greater equal than from index and smaller equal than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 1, toIndex: 4 })).toThrow(
+      /to index must be a integer greater equal than from index and smaller equal than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 1.5, toIndex: 2 })).toThrow(
+      /start index must be a integer greater than 0 and smaller equal than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 0, toIndex: 1.5 })).toThrow(
+      /to index must be a integer greater equal than from index and smaller equal than length/,
+    );
   });
 });
--- packages/array-max/src/index.js
+++ packages/array-max/src/index.js
@@ -3,9 +3,11 @@ import isArray from 'is-any-array';
 /**
  * Computes the maximum of the given values
  * @param {Array<number>} input
+ * @param {number} [options.fromIndex] - Start index (inclusive) for the slice within which we look for the max
+ * @param {number} [options.toIndex] - End index (exclusive) for the slice within which we look for the max
  * @return {number}
  */
-export default function max(input) {
+export default function max(input, options = {}) {
   if (!isArray(input)) {
     throw new TypeError('input must be an array');
   }
@@ -14,8 +16,33 @@ export default function max(input) {
     throw new TypeError('input must not be empty');
   }
 
-  let maxValue = input[0];
-  for (let i = 1; i < input.length; i++) {
+  let { fromIndex, toIndex } = options;
+  if (fromIndex === undefined) {
+    fromIndex = 0;
+  } else if (
+    (fromIndex < 0) |
+    (fromIndex > input.length) |
+    !Number.isInteger(fromIndex)
+  ) {
+    throw new Error(
+      'start index must be a integer greater than 0 and smaller equal than length',
+    );
+  }
+
+  if (toIndex === undefined) {
+    toIndex = input.length;
+  } else if (
+    (toIndex <= fromIndex) |
+    (toIndex > input.length) |
+    !Number.isInteger(toIndex)
+  ) {
+    throw new Error(
+      'to index must be a integer greater equal than from index and smaller equal than length',
+    );
+  }
+
+  let maxValue = input[fromIndex];
+  for (let i = fromIndex + 1; i < toIndex; i++) {
     if (input[i] > maxValue) maxValue = input[i];
   }
   return maxValue;
